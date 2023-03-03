5a784a2f62b33474a8584b8d737f0838baa9f6f7
Caio Kawasaki <caio@rilo.com.br>
2023-03-03 18:30:24 -0300

    Doc blocks updated

--- src/Facades/Sftp.php
+++ src/Facades/Sftp.php
@@ -6,6 +6,8 @@ use Illuminate\Support\Facades\Facade;
 
 /**
  * @method static \Nulvem\Remote\Connections\Sftp preserveDates(bool $preserveDates = true)
+ * @method static \Nulvem\Remote\Connections\Sftp on(string $host)
+ * @method static \Nulvem\Remote\Connections\Sftp setTimeout(int $timeout)
  * @method static \Nulvem\Remote\Clients\Outputs\SftpOutput get(string $from, string $to = null)
  * @method static \Nulvem\Remote\Clients\Outputs\SftpOutput put(string $from, string $to = '/root')
  */
--- src/Facades/Ssh.php
+++ src/Facades/Ssh.php
@@ -5,6 +5,8 @@ namespace Nulvem\Remote\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
+ * @method static \Nulvem\Remote\Connections\Ssh on(string $host)
+ * @method static \Nulvem\Remote\Connections\Ssh setTimeout(int $timeout)
  * @method static \Nulvem\Remote\Clients\Outputs\SshOutput run(string $script, array $data = [])
  */
 class Ssh extends Facade
